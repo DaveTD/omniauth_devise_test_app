7837080c41a9cf0ca25f2f5093fe1a8a4d7d1178
DaveTD <its.dave.dawson@gmail.com>
2014-06-18 00:41:56 -0400

    Clean up abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,9 +4,8 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
 
-    alias_action :create, :update, :destroy, :new, :show, :index, :to => :incuds
-    alias_action :create, :new, :update, :destroy, :to => :private_actions
-    alias_action :read, :show, :index, :to => :public_actions
+    alias_action :create, :update, :destroy, :new, :read, :to => :crud
+    alias_action :update, :destroy, :to => :private_actions
 
     # public actions
     can :public_actions, UserType1
@@ -16,29 +15,36 @@ class Ability
     can :create, UserType2
     can :new_type1, User
     can :new_type2, User
+    can :index, :user_type1
 
     case user.type_type
     when "Admin"
       can :manage, :all
     when "UserType1"  
       can :manage, :protected_static_page
-      can :read, UserType1
-
       can :manage, UserType1 do |u|
         u == user.type
       end
       can :manage, User do |u|
         u == user
       end
+      
+      can :add_twitter_uid, :user_type1       
+      can :add_facebook_uid, :user_type1       
+      can :destroy_twitter, :user_type1
+      can :destroy_facebook, :user_type1
 
-      can :public_actions, [:thing1, :thing2, :thing3] 
+      can :read, [:thing1, :thing2, :thing3] 
       can :private_actions, [:thing1, :thing2, :thing3]
+      can :crud, [:thing1, :thing2, :thing3]
 
     when "UserType2"
-      can :manage, UserType2
       can :manage, :protected_static_page
+      can :manage, UserType2 do |u|
+        u == user.type
+      end
 
-      can :public_actions, [:thing1, :thing2, :thing3]
+      can :read, [:thing1, :thing2, :thing3]
     end
   end
 end
